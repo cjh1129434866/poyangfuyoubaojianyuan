727ab1e2010a8b34def62758817ba2a3537db5ad
tigerChen <1129434866@qq.com>
2020-09-28 19:43:03 +0800

    添加备案号

--- components/copyRight.vue
+++ components/copyRight.vue
@@ -2,7 +2,7 @@
 	<view 
 		class="right u-text-center u-padding-top-30 u-padding-bottom-30 u-font-26" 
 		style="background: #423530; color: #fff; position: fixed; left: 0; bottom: 0;width: 100%; z-index: 9999;">
-		<u-link href="http://www.baidu.com"></u-link> 技术支持：江西省仁煦科技有限公司
+		<text class="u-font-10">医院备案号:&nbsp;赣ICP备2020011721号</text><br/><text>技术支持：江西省仁煦科技有限公司</text>
 	</view>
 </template>
 
